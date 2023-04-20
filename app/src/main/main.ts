6152e2419165cc194ede09988c5c0933275794d8
theronburger <dev@theronburger.com>
2023-04-20 16:26:16 +0200

    feat(ui): Use custom toolbar

--- app/src/main/main.ts
+++ app/src/main/main.ts
@@ -79,6 +79,12 @@ const createWindow = async () => {
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
     },
+    titleBarStyle: 'hidden',
+    titleBarOverlay: {
+      color: '#2f3241',
+      symbolColor: '#74b1be',
+      height: 50,
+    },
   });
 
   mainWindow.loadURL(resolveHtmlPath('index.html'));
