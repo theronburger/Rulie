7aed66b065112a93f5d7002a15f204aec68e8de4
theronburger <dev@theronburger.com>
2023-04-24 12:49:02 +0200

    feat(core): Creating rulieCore in main.ts

--- app/src/main/main.ts
+++ app/src/main/main.ts
@@ -9,11 +9,12 @@
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
 import path from 'path';
-import { app, BrowserWindow, shell, ipcMain } from 'electron';
+import { app, BrowserWindow, shell } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import MenuBuilder from './menu';
 import { resolveHtmlPath } from './util';
+import RulieCore from './rulie/rulieCore';
 
 class AppUpdater {
   constructor() {
@@ -23,13 +24,9 @@ class AppUpdater {
   }
 }
 
-let mainWindow: BrowserWindow | null = null;
+const rulieCore = new RulieCore();
 
-ipcMain.on('ipc-example', async (event, arg) => {
-  const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
-  console.log(msgTemplate(arg));
-  event.reply('ipc-example', msgTemplate('pong'));
-});
+let mainWindow: BrowserWindow | null = null;
 
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support');
